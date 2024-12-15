851d0afcc9bc911efe857c5550666b3d4f05422b
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-14 22:56:16 -0500

    Ejemplo de tipo function <T,R>

--- Examples/Main.java
+++ Examples/Main.java
@@ -1,6 +1,6 @@
 package Examples;
 
-import java.util.function.Predicate;
+import java.util.function.*;
 
 public class Main {
 
@@ -10,11 +10,19 @@ public class Main {
         // ejemplo numero par o impar
         Predicate<Integer> esPar = numero -> numero % 2 == 0;
         // mostramos el resultado
-        System.out.println("Es Par: "+esPar.test(12));
+        System.out.println("Es Par: " + esPar.test(12));
         Predicate<Integer> mayorEdad = edad -> edad >= 18;
         System.out.println("Es mayor edad: " + mayorEdad.test(13));
         System.out.println("---------------------------");
-     
+
+        /* EJEMPLO DE FUNCTION <T,R> */
+        System.out.println("\nEJEMPLO DE FUNCTION <T,R> ");
+        Function<String, Integer> longitudPalabra = letra -> letra.length();
+        System.out.println("Tamanio de la palabra: "+longitudPalabra.apply("Hola"));
+        Function<Integer,Boolean> esMayorEdad = edad -> edad >= 18;
+        System.out.println("Es mayor de edad: "+esMayorEdad.apply(12));
+        System.out.println("---------------------------");
+
     }
 
 }
